e316ce22398d522fb52add7dabb53087825d3e1a
B-Nilson <nilson.brayden@gmail.com>
2026-03-16 22:52:25 -0700

    extend check_installed() to aermap and aermet components

--- R/check_installed.R
+++ R/check_installed.R
@@ -1,63 +1,87 @@
-#' Check if AERMOD is installed
+#' Check if AERMOD, AERMAP, or AERMET is installed
 #'
-#' @param path Path to AERMOD executable (optional). If provided and does not end in `exe_name`, it will be appended.
-#' @param exe_name Name of the AERMOD executable (default: "aermod.exe" on Windows, "aermod" otherwise)
+#' @param path Path to the model executable (optional). If provided and does not end in `exe_name`, it will be appended.
+#' @param model Name of the model (default: "aermod", must be one of "aermod", "aermap", or "aermet")
+#' @param exe_name Name of the model executable (default: "aermod.exe" on Windows, "aermod" otherwise)
 #' @param verbose Whether to print a success message if AERMOD is found (default: TRUE)
 #' @return A list containing the found path, the source of the path (either "user", "PATH", or "common_path"), and whether the path was found.
 #' @export
 check_installed <- function(
   path = NULL,
-  exe_name = ifelse(.Platform$OS.type == "windows", "aermod.exe", "aermod"),
+  model = c("aermod", "aermap", "aermet")[1],
+  exe_name = model |>
+    paste0(ifelse(.Platform$OS.type == "windows", ".exe", "")),
   verbose = TRUE
 ) {
+  stopifnot(
+    "`path` must be NULL or a character vector of length 1" = path |>
+      is.character() &&
+      length(path) == 1 ||
+      is.null(path),
+    "`model` must be one of 'aermod', 'aermap', or 'aermet'" = model %in%
+      c("aermod", "aermap", "aermet") &
+      length(model) == 1,
+    "`exe_name` must be a character vector of length 1" = exe_name |>
+      is.character() &&
+      length(exe_name) == 1,
+    "`verbose` must be a logical vector of length 1" = verbose |>
+      is.logical() &&
+      length(verbose) == 1
+  )
+
   if (!is.null(path)) {
     path <- ifelse(
       path |> endsWith(exe_name),
       path,
       file.path(path, exe_name)
     )
-    install_test <- path |> check_user_path(verbose = verbose)
+    install_test <- path |>
+      check_user_path(model = model, verbose = verbose)
     if (!is.null(install_test)) {
       return(install_test)
     }
   }
 
-  install_test <- exe_name |> check_PATH(verbose = verbose)
+  install_test <- exe_name |>
+    check_PATH(model = model, verbose = verbose)
   if (!is.null(install_test)) {
     return(install_test)
   }
 
-  install_test <- exe_name |> check_common_locations(verbose = verbose)
+  install_test <- exe_name |>
+    check_common_locations(model = model, verbose = verbose)
   if (!is.null(install_test)) {
     return(install_test)
   }
 
   PATH <- Sys.getenv('PATH')
+  model <- toupper(model)
   cli::cli_warn(c(
-    "AERMOD executable not found.",
+    "{model} executable not found.",
     "• Checked for {exe_name} in:",
     "- PATH ({PATH})",
     "- common install locations",
-    if (!is.null(path)) "- supplied path {.path path}",
+    if (!is.null(path)) "- supplied path {.path {path}}",
     "• To fix:",
-    "1. Install AERMOD from the U.S. EPA:",
+    "1. Install {model} from the U.S. EPA:",
     "https://www.epa.gov/scram/air-quality-dispersion-modeling-preferred-and-recommended-models#aermod",
     "2. Add the folder containing '{exe_name}' to your system PATH if not already included",
     "or supply it explicitly:",
-    "`check_installed(path = \"C:/path/to/aermod.exe\")`"
+    "`check_installed(path = \"C:/path/to/{exe_name}\")`"
   ))
 }
 
-check_user_path <- function(path, verbose = TRUE) {
+check_user_path <- function(path, model, verbose = TRUE) {
   is_exec <- \(p) {
     !is.null(p) && nzchar(p) && file.exists(p) && file.access(p, mode = 1)
   }
   path <- path |>
     normalizePath(winslash = "/", mustWork = FALSE)
+  model <- toupper(model)
 
   if (is_exec(path)) {
     if (verbose) {
-      cli::cli_alert_success("AERMOD found at {.path {path}}")
+      cli::cli_alert_success("{model} found at {.path {path}}")
     }
     list(
       found = TRUE,
@@ -65,17 +89,18 @@ check_user_path <- function(path, verbose = TRUE) {
       source = "user"
     )
   } else {
-    cli::cli_warn("Supplied AERMOD path does not exist: {.path {path}}")
+    cli::cli_warn("Supplied {model} path does not exist: {.path {path}}")
     invisible()
   }
 }
 
-check_PATH <- function(exe_name, verbose = TRUE) {
+check_PATH <- function(exe_name, model, verbose = TRUE) {
   path_found <- Sys.which(exe_name)
 
   if (nzchar(path_found)) {
     if (verbose) {
-      cli::cli_alert_success("AERMOD found at {.path {path}}")
+      model <- toupper(model)
+      cli::cli_alert_success("{model} found at {.path {path}}")
     }
     list(
       found = TRUE,
@@ -87,21 +112,22 @@ check_PATH <- function(exe_name, verbose = TRUE) {
   }
 }
 
-check_common_locations <- function(exe_name, verbose = TRUE) {
+check_common_locations <- function(exe_name, model, verbose = TRUE) {
   candidates <- c(
     # Windows
-    "C:/AERMOD/aermod.exe",
-    "C:/Program Files/AERMOD/aermod.exe",
-    "C:/Program Files (x86)/AERMOD/aermod.exe",
+    "C:/AERMOD/",
+    "C:/Program Files/AERMOD/",
+    "C:/Program Files (x86)/AERMOD/",
     # Linux etc
-    "/usr/local/bin/aermod",
-    "/usr/bin/aermod",
-    "/opt/aermod/bin/aermod",
-    "/usr/lib/aermod/bin/aermod",
-    "~/aermod/aermod",
-    "/usr/local/aermod/bin/aermod",
-    "/usr/local/aermod-*/bin/aermod"
-  )
+    "/usr/local/bin/",
+    "/usr/bin/",
+    "/opt/aermod/bin/",
+    "/usr/lib/aermod/bin/",
+    "~/aermod/",
+    "/usr/local/aermod/bin/",
+    "/usr/local/aermod-*/bin/"
+  ) |>
+    paste0(exe_name)
 
   candidates <- candidates |>
     normalizePath(winslash = "/", mustWork = FALSE)
@@ -111,7 +137,8 @@ check_common_locations <- function(exe_name, verbose = TRUE) {
   if (length(existing) > 0) {
     path <- existing[1]
     if (verbose) {
-      cli::cli_alert_success("AERMOD found at {.path {path}}")
+      model <- toupper(model)
+      cli::cli_alert_success("{model} found at {.path {path}}")
     }
     list(
       found = TRUE,
--- tests/testthat/test-check_installed.R
+++ tests/testthat/test-check_installed.R
@@ -1,19 +1,34 @@
 test_that("warning when not installed, no warnings when installed", {
-  not_installed <- check_installed("C:/", verbose = FALSE) |>
-    expect_warning(
-      regexp = "(AERMOD executable not found)|(Supplied AERMOD path does not exist)"
-    )
+  models <- c("aermod", "aermap", "aermet")
+  for (model in models) {
+    not_installed <- "C:/" |>
+      check_installed(model = !!model, verbose = FALSE) |>
+      expect_warning(
+        regexp = "(%s executable not found)" |>
+          sprintf(toupper(model))
+      ) |>
+      expect_warning(
+        regexp = "(Supplied %s path does not exist)" |>
+          sprintf(toupper(model))
+      )
 
-  installed <- check_installed() |>
-    expect_message(regexp = "AERMOD found at")
+    installed <- check_installed() |>
+      expect_message(
+        regexp = "%s found at" |>
+          sprintf(toupper(model))
+      )
+  }
 })
 
 test_that("installed object formatted correctly", {
-  installed <- check_installed(verbose = FALSE) |>
-    expect_named(c("found", "path", "source")) |>
-    expect_type("list")
+  models <- c("aermod", "aermap", "aermet")
+  for(model in models) {
+    installed <- check_installed(model = !!model, verbose = FALSE) |>
+      expect_named(c("found", "path", "source")) |>
+      expect_type("list")
 
-  expect_true(installed$found)
-  expect_type(installed$path, "character")
-  expect_type(installed$source, "character")
+    expect_true(installed$found)
+    expect_type(installed$path, "character")
+    expect_type(installed$source, "character")
+  }
 })
