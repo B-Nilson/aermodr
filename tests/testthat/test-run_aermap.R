e8d053cf176816a824afe21340ac7455225b3c63
Brayden Nilson <nilson.brayden@gmail.com>
2026-03-17 22:18:31 -0700

    remove erroneous models def

--- tests/testthat/test-run_aermap.R
+++ tests/testthat/test-run_aermap.R
@@ -2,8 +2,7 @@ test_that("able to run test case", {
   path <- tempdir() |> file.path("aermod_project")
   on.exit(unlink(path, recursive = TRUE))
 
-  models <- c("aermod", "aermap", "aermet")
-  path |> setup_test_case(verbose = FALSE, prompt = FALSE)
+  path |> setup_test_case(verbose = FALSE, prompt = FALSE, open = FALSE)
 
   result <- path |>
     paste0("/AERMAP/") |> 
